35752a2fa31c113c0333faae3ba3443c75e3d9fe
SeraphJACK <seraphjack@outlook.com>
2020-08-06 20:51:54 +0800

    Thonk

--- main.go
+++ main.go
@@ -161,7 +161,7 @@ func main() {
 	router := httprouter.New()
 	router.GET("/dimension", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		var arr []string
-		for key, _ := range dimensions {
+		for key := range dimensions {
 			arr = append(arr, key)
 		}
 		data, _ := json.Marshal(arr)
@@ -260,7 +260,7 @@ func main() {
 			PushType:   PushTypeDelete,
 			Identifier: identifier,
 			Dim:        dim,
-			Wp:         nil,
+			Wp:         Waypoint{},
 		})
 	})
 
